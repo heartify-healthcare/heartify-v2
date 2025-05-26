870eee10f58ebf1321e379be9b3ca046f77f4b37
Vo Tran Phi <songokupkj@gmail.com>
2025-05-27 03:35:14 +0700

    feat(h-02): new Navigation Bar and (tabs) folder for file-based routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="signup" options={{ headerShown: false }} />
       <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
       <Stack.Screen name="verify-otp" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
