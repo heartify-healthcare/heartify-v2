698229923f7e1dc0fe1bd5a439b5b7e1800a1732
Vo Tran Phi <songokupkj@gmail.com>
2025-10-27 01:20:56 +0700

    chore(hv-02): remove all predictions screen content

--- components/predictions/index.ts
+++
@@ -1,3 +0,0 @@
-export { PredictionCard } from './PredictionCard';
-export type { PredictionData } from './types';
-export { formatSex, formatProbability, formatTimestamp, getPredictionColor } from './utils';
