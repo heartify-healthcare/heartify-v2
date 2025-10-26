09e9afadbd200f5b131ffc1a6ccc878f7da80f9e
Vo Tran Phi <songokupkj@gmail.com>
2025-10-27 03:10:41 +0700

    fix(hv-02): remove html tag in ecg placeholder screen

--- app/(tabs)/ecg.tsx
+++ app/(tabs)/ecg.tsx
@@ -2,10 +2,7 @@ import { styles } from '@/styles/(tabs)/ecg';
 
 const ECGScreen: React.FC = () => {
   return (
-    <div>
-      <h1>ECG</h1>
-      {/* ECG content goes here */}
-    </div>
+    <></>
   );
 };
 
